d054b3b513fc1f3af31d9428b65f35b9e09c5f96
Dmitrij Mosejko <znaten@gmail.com>
2019-10-29 09:56:14 +0300

    developing model object

--- script.js
+++ script.js
@@ -1,14 +1,58 @@
 let view = {
-    displayMessage: function(msg) {
+    displayMessage: function (msg) {
         let messageArea = document.getElementById("messageArea");
         messageArea.innerHTML = msg;
     },
-    displayHit: function(location) {
+    displayHit: function (location) {
         let cell = document.getElementById(location);
         cell.setAttribute("class", "hit");
     },
-    displayMiss: function(location) {
+    displayMiss: function (location) {
         let cell = document.getElementById(location);
         cell.setAttribute("class", "miss");
     },
+};
+
+let model = {
+    boardSize: 7,
+    numShips: 3,
+    shipsSunk: 0,
+    shipLength: 3,
+    ships: [{locations: ["06", "16", "26"], hits: ["", "", ""]},
+            {locations: ["24", "34", "44"], hits: ["", "", ""]},
+            {locations: ["10", "11", "12"], hits: ["", "", ""]}],
+    fire: function (guess) {
+        for (let i = 0; i < this.numShips; i++) {
+            let ship = this.ships[i];
+            let index = ship.locations.indexOf(guess);
+            if (index >= 0) {
+                ship.hits[index] = "hit";
+                view.displayHit(guess);
+                view.displayMessage("Попадание!");
+                if (this.isSunk(ship)) {
+                    view.displayMessage("Вы потопили корабль!");
+                    this.shipsSunk++;
+                }
+                return true;
+            }
+        }
+        view.displayMiss(guess);
+        view.displayMessage("Промах");
+        return false;
+    },
+    isSunk: function(ship) {
+        for (let i = 0; i < this.shipLength; i++) {
+            if (ship.hits !== "hit") {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+let controller = {
+    guesses: 0,
+    processGuess: function(guess) {
+
+    }
 };
